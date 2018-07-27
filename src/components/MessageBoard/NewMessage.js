08d52e665208b0fd7b7618b24b4ce18d9497cad0
ali <mohsennbarzegar@gmail.com>
2018-07-27 13:33:43 +0430

    less ugly new message component

--- src/components/MessageBoard/NewMessage.js
+++ src/components/MessageBoard/NewMessage.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Modal,
+  Button,
+} from 'react-bootstrap';
 
 import { sendMessage } from '../../actions/board';
 
@@ -7,12 +14,14 @@ class NewMessage extends Component {
 
   constructor(props) {
       super(props);
-      this.state = { message: '', selectedTags: [] };
+      this.state = { message: '', selectedTags: [], show: false };
 
       this.handleChange = this.handleChange.bind(this);
       this.onSendMessageHandler = this.onSendMessageHandler.bind(this);
       this.renderTagList = this.renderTagList.bind(this);
       this.handleTagSelect = this.handleTagSelect.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+      this.handleShow = this.handleShow.bind(this);
   }
 
   onSendMessageHandler() {
@@ -51,31 +60,55 @@ class NewMessage extends Component {
     this.setState({ selectedTags: tags });
   }
 
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
+
     return(
       <div>
-        <div>
-          <h3>پیام جدید</h3>
-          <textarea
-            type="text"
-            name="message"
-            placeholder="type your message"
-            value={this.state.message}
-            onChange={this.handleChange}
-            maxLength={300}
-            />
+        <button onClick={this.handleShow} >
+          ارسال پیام جدید
+        </button>
 
-        </div>
-        <div>
-          <select name="" onChange={this.handleTagSelect} multiple>
-            { this.props.tags === undefined ? '' :
-              this.props.tags.map( (tag) => this.renderTagList(tag) )
-            }
-          </select>
-        </div>
-        <div>
-          <button onClick={this.onSendMessageHandler}>send</button>
-        </div>
+        <Modal show={this.state.show} onHide={this.handleClose} dir='rtl'>
+          <Modal.Header closeButton>
+            <Modal.Title>پیام جدید</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormGroup
+              controlId="formControlsTextarea"
+              type="text"
+              name="message"
+              placeholder="type your message"
+              value={this.state.message}
+              onChange={this.handleChange}
+              maxLength={300}>
+              <FormControl componentClass="textarea" placeholder="پیام خود را اینجا بنویسید" />
+            </FormGroup>
+            <div>
+              <FormGroup controlId="formControlsSelectMultiple">
+                <ControlLabel>تگ ها</ControlLabel>
+                <FormControl componentClass="select" onChange={this.handleTagSelect} multiple>
+                  { this.props.tags === undefined ? '' :
+                    this.props.tags.map( (tag) => this.renderTagList(tag) )
+                  }
+                </FormControl>
+              </FormGroup>
+            </div>
+            <div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.onSendMessageHandler}>ارسال</Button>
+          </Modal.Footer>
+        </Modal>
+
       </div>
     );
   };
