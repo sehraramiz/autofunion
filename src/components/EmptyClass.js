93ee7787ed3ef61bc060f339fdb3bb8e71adca79
ali <mohsennbarzegar@gmail.com>
2018-08-07 20:58:16 +0430

    minor text change in EmptyClass

--- src/components/EmptyClass.js
+++ src/components/EmptyClass.js
@@ -9,10 +9,10 @@ class EmptyClass extends React.Component {
   render() {
     return (
       <div className="table-page-container" dir="rtl">
-        <h2>کلاسای خالی هفته</h2>
-        <h3>چیزی که اگه از نگهبان آمارش و بخوای میگه باید از امور فرهنگی نامه بیاری :|</h3>
-        <h4>اینا که کلاسای پره!</h4>
-        <h5>از اصل عدم شمول استفاده کن عزیزم</h5>
+        <p>کلاسای خالی هفته</p>
+        <p>چیزی که اگه از نگهبان آمارش و بخوای میگه باید از امور فرهنگی نامه بیاری :|</p>
+        <p>- اینا که کلاسای پره!</p>
+        <p>+ از اصل عدم شمول استفاده کن عزیزم</p>
         <Table allClasses={getAllClassesArray()} squareContent="location"/>
       </div>
     );
